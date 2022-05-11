43a8c23d1e33ec490a855ebc9db1120563b8c86e
ja3y <josephikeh1@gmail.com>
2022-05-11 23:06:34 +0100

    objectives

--- Terrable.py
+++ Terrable.py
@@ -1,3 +1,7 @@
 #This is a python automation for Terraform & Ansible
+#install terraform and Ansible 
+#call and execute terraform script
+#After infrastructures deploys, python copy’s Ansible pub keys to the servers
+#After copying keys, it executes a ping and install Apache server
 import os
 import subprocess
