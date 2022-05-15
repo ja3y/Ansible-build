f4d12120fa6115a142f17e69458b57fcf8e2ae32
ja3y <josephikeh1@gmail.com>
2022-05-15 23:02:20 +0100

    Functions

--- Terrable.py
+++ Terrable.py
@@ -12,18 +12,28 @@ print ("---------Welcome---------")
 #terrainstall = (os.system("sudo apt-get install terraform"))
 #ansinstall = (os.system("sudo apt-get install ansible"))
 
-checkansiversion =  (os.system("ansible --version > ansiblecheck.txt"))
+checkansiversion =  (os.system("ansible --version |grep core > ansiblecheck.txt"))
 checkterraversion = (os.system("terraform -v > terraformcheck.txt"))
 
 with open('ansiblecheck.txt') as chanv :
-    chanvlines = chanv.read()
+    chanvlines = chanv.readlines()
 with open('terraformcheck.txt') as chter :
-    chterlines = chter.read()
-for line in chanvlines:
+    chterlines = chter.readlines()
+for line in chanvlines and  chterlines:
 
     if str("ansible [core 2.12.4]") in line: ##check for readlines command in python
         print ("[+] Ansible Is Installed on Host")
     else:
-        print ("[+] Ansible is not installed, install Ansible with the command 'sudo apt-get install ansible'")
-    if str("Terraform") in line:
-        print ("[+] Terraform is installed") ##check why Terraf
+        print ("[-] Ansible is not installed, install Ansible with the command 'sudo apt-get install ansible'")
+    if str("Terraform v") in line:
+        print ("[+] Terraform is installed")
+    else:  ##check why Terraform
+        print ("[-]Terraform is not installed, install Terraform with the command 'sudo apt-get install Terraform'")
+
+
+def terracall():
+    os.system("terraform init")
+
+
+
+terracall()
